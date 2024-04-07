d403b30760cca792022160072c3a60c9b8218e85
gumquat <gumquat@gmail.com>
2024-04-07 13:41:07 -0500

    made python file that pushes json data to database

--- json2Postgres_01.py
+++ json2Postgres_01.py
@@ -13,18 +13,22 @@ conn = psycopg2.connect(
 # Create a cursor object
 cur = conn.cursor()
 
-# Open the JSON file
+# Open the JSON file and load the data
 with open('drinks.json', 'r') as f:
-    # Load the JSON data
-    drinks_data = json.load(f)
+    data = json.load(f)
 
-# Insert the JSON data into the table
-# MAKE SURE THAT THE LAST %S imports AN ARRAY!!!!!!!!! <---
-for drink in drinks_data:
-    cur.execute("""
-        INSERT INTO drinks (name, description, price, drink_type, ingredients)
-        VALUES (%s, %s, %s, %s, %s) 
-    """, (drink['name'], drink['description'], drink['price']))
+# Insert the data into the 'drinks' table
+for drink in data:
+    sql = "INSERT INTO drinks (drink_id, drink_name, description, price, drink_type, ingredients) VALUES (%s, %s, %s, %s, %s, %s)"
+    values = (
+        drink['drink_id'],
+        drink['drink_name'],
+        drink['description'],
+        drink['price'],
+        drink['drink_type'],
+        drink['ingredients']
+    )
+    cur.execute(sql, values)
 
 # Commit the changes and close the connection
 conn.commit()
