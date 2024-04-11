9b952ab5b150cf14d6a2d78c9219a8c609321459
evanrich2404 <evanrich2404@gmail.com>
2024-04-11 15:28:30 -0500

    Starting my logger stuff, Making pycodestyle formatting because lookgood, May have gone over-kill with the logger stuff but just wanted to see what it all did and they seem all very useful!, I setup the log format then setup the config to output to a specific place(it will make it if it isn't there) and I have it set to DEBUG to show DEBUG level and above up to CRITICAL level (there are 5 default/constant levels to logging) then I initialized the logger function and made it specify that it was the logger for app (appLogger) so it should show a log like so: INFO timedate - appLogger - Success then I put messages to show the Flask app running and then to tell it is loading the config Then went ahead and wrapped the postgres connection to let us know it had an issue or not Then I added 3 handlers: before_request after_request handle_exception Which in order: It will tell you the request being handled, tell you what happened when handling the request, catch any unhandled exceptions Then I did some basic logging for each part of the app afterwards Dunno if I need to do anything for the ```if __name__ == '__main__':``` area so I didn't touch :)

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_caching import Cache
 import psycopg2
+import logging
+
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(name)s - %(message)s"
+logging.basicConfig(filename='./logs/app.log',
+                    level=logging.DEBUG,
+                    format=LOG_FORMAT,
+                    filemode='a')
+logger = logging.getLogger('appLogger')
 
 app = Flask(__name__)
+
+logger.info("Starting Flask application")
+logger.info("Loading configuration...")
 
 # Configure the cache
 app.config.from_mapping(
@@ -12,148 +23,193 @@ app.config.from_mapping(
 cache = Cache(app)
 
 # Connect to the PostgreSQL database
-conn = psycopg2.connect(
-    dbname="space_bar",
-    user="postgres",
-    password="Password123",
-    host="localhost",
-    port="5432"
-)
+try:
+    conn = psycopg2.connect(
+        dbname="space_bar",
+        user="postgres",
+        password="Password123",
+        host="localhost",
+        port="5432"
+    )
+    logger.info("Successfully connected to the database.")
+except psycopg2.OperationalError as e:
+    logger.critical(f"Failed to connect to the database: {e}")
 
 # Create a cursor object
 cur = conn.cursor()
 
-### ROUTE ::: ALL DRINKS ###
+
+@app.before_request
+def before_request_log():
+    logger.debug(f"Handling request: {request.path}")
+
+
+@app.after_request
+def after_request_log(response):
+    logger.debug(f"Handled request: {request.path} -
+                 Response status: {response.status_code}")
+    return response
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    logger.error(f"Unhandled exception: {e}", exc_info=True)
+    return "An internal error occured.", 500
+
+
+# ### ROUTE ::: ALL DRINKS ###
 @app.route('/drinks', methods=['GET'])
 @cache.cached(timeout=300)
-
 def get_all_drinks():
     """Route that GETS all drinks from entire database
     """
-    # Query the "drinks" table to get all data
-    cur.execute("SELECT * FROM drinks")
-    drinks = cur.fetchall()
+    logger.info("Fetching all drinks")
+    try:
+        # Query the "drinks" table to get all data
+        cur.execute("SELECT * FROM drinks")
+        drinks = cur.fetchall()
 
-    # Convert the query results to a list of dictionaries
-    drink_list = []
-    for drink in drinks:
-        drink_dict = {
-            'drink_id': drink[0],
-            'drink_name': drink[1],
-            'description': drink[2],
-            'price': drink[3],
-            'drink_type': drink[4],
-            'ingredients': drink[5]
-        }
-        drink_list.append(drink_dict)
+        # Convert the query results to a list of dictionaries
+        drink_list = []
+        for drink in drinks:
+            drink_dict = {
+                'drink_id': drink[0],
+                'drink_name': drink[1],
+                'description': drink[2],
+                'price': drink[3],
+                'drink_type': drink[4],
+                'ingredients': drink[5]
+            }
+            drink_list.append(drink_dict)
 
-    return jsonify(drink_list)
+        return jsonify(drink_list)
+    except Exception as e:
+        logger.error(f"Failed to fetch drinks: {e}")
+        return "An error occured!", 500
 
 
-### ROUTE ::: COCKTAILS ###
+# ### ROUTE ::: COCKTAILS ###
 @app.route('/cocktails', methods=['GET'])
 @cache.cached(timeout=300)
-
 def get_cocktails():
     """Route that GETS all drinks of type 'cocktail'
     """
-    # Query the "drinks" table for drinks with a drink_type of 'cocktail'
-    cur.execute("SELECT * FROM drinks WHERE drink_type = 'Cocktail'")
-    cocktails = cur.fetchall()
+    logger.info("Fetching Cocktails")
+    try:
+        # Query the "drinks" table for drinks with a drink_type of 'cocktail'
+        cur.execute("SELECT * FROM drinks WHERE drink_type = 'Cocktail'")
+        cocktails = cur.fetchall()
 
-    # Convert the query results to a list of dictionaries
-    cocktail_list = []
-    for cocktail in cocktails:
-        cocktail_dict = {
-            'drink_id': cocktail[0],
-            'drink_name': cocktail[1],
-            'description': cocktail[2],
-            'price': cocktail[3],
-            'drink_type': cocktail[4],
-            'ingredients': cocktail[5]
-        }
-        cocktail_list.append(cocktail_dict)
+        # Convert the query results to a list of dictionaries
+        cocktail_list = []
+        for cocktail in cocktails:
+            cocktail_dict = {
+                'drink_id': cocktail[0],
+                'drink_name': cocktail[1],
+                'description': cocktail[2],
+                'price': cocktail[3],
+                'drink_type': cocktail[4],
+                'ingredients': cocktail[5]
+            }
+            cocktail_list.append(cocktail_dict)
 
-    return jsonify(cocktail_list)
+        return jsonify(cocktail_list)
+    except Exception as e:
+        logger.error(f"Failed to fetch cocktails: {e}")
+        return "An error occured!", 500
 
-### ROUTE ::: COCKTAILS ###
+
+# ### ROUTE ::: COCKTAILS ###
 @app.route('/beers', methods=['GET'])
 @cache.cached(timeout=300)
-
 def get_beers():
     """Route that GETS all drinks of type 'beer'
     """
-    # Query the "drinks" table for drinks with a drink_type of 'beer'
-    cur.execute("SELECT * FROM drinks WHERE drink_type = 'Beer'")
-    beers = cur.fetchall()
+    logger.info("Fetching Beers")
+    try:
+        # Query the "drinks" table for drinks with a drink_type of 'beer'
+        cur.execute("SELECT * FROM drinks WHERE drink_type = 'Beer'")
+        beers = cur.fetchall()
 
-    # Convert the query results to a list of dictionaries
-    beer_list = []
-    for beer in beers:
-        beer_dict = {
-            'drink_id': beer[0],
-            'drink_name': beer[1],
-            'description': beer[2],
-            'price': beer[3],
-            'drink_type': beer[4],
-            #'ingredients': beer[5]
-        }
-        beer_list.append(beer_dict)
+        # Convert the query results to a list of dictionaries
+        beer_list = []
+        for beer in beers:
+            beer_dict = {
+                'drink_id': beer[0],
+                'drink_name': beer[1],
+                'description': beer[2],
+                'price': beer[3],
+                'drink_type': beer[4],
+                # 'ingredients': beer[5]
+            }
+            beer_list.append(beer_dict)
 
-    return jsonify(beer_list)
+        return jsonify(beer_list)
+    except Exception as e:
+        logger.error(f"Failed to fetch beers: {e}")
+        return "An error occured!", 500
 
 
-### ROUTE ::: WINE ###
+# ### ROUTE ::: WINE ###
 @app.route('/wines', methods=['GET'])
 @cache.cached(timeout=300)
-
 def get_wines():
     """Route that GETS all drinks of type 'wine'
     """
-    # Query the "drinks" table for drinks with a drink_type of 'wine'
-    cur.execute("SELECT * FROM drinks WHERE drink_type = 'Wine'")
-    wines = cur.fetchall()
+    logger.info("Fetching Wines")
+    try:
+        # Query the "drinks" table for drinks with a drink_type of 'wine'
+        cur.execute("SELECT * FROM drinks WHERE drink_type = 'Wine'")
+        wines = cur.fetchall()
 
-    # Convert the query results to a list of dictionaries
-    wine_list = []
-    for wine in wines:
-        wine_dict = {
-            'drink_id': wine[0],
-            'drink_name': wine[1],
-            'description': wine[2],
-            'price': wine[3],
-            'drink_type': wine[4],
-            'ingredients': wine[5]
-        }
-        wine_list.append(wine_dict)
+        # Convert the query results to a list of dictionaries
+        wine_list = []
+        for wine in wines:
+            wine_dict = {
+                'drink_id': wine[0],
+                'drink_name': wine[1],
+                'description': wine[2],
+                'price': wine[3],
+                'drink_type': wine[4],
+                'ingredients': wine[5]
+            }
+            wine_list.append(wine_dict)
 
-    return jsonify(wine_list)
+        return jsonify(wine_list)
+    except Exception as e:
+        logger.error(f"Failed to fetch wines: {e}")
+        return "An error occured!", 500
 
-### ROUTE ::: BUDGET_DRINKS ###
+
+# ### ROUTE ::: BUDGET_DRINKS ###
 @app.route('/budget_drinks', methods=['GET'])
 @cache.cached(timeout=300)
-
 def get_budget_drinks():
     """Route that GETS drinks with a price value of or less than '10.99'
     """
-    # Query the "drinks" table for drinks with a price of 10.99 or lower
-    cur.execute("SELECT * FROM drinks WHERE price::numeric <= 10.99")
-    budget_drinks = cur.fetchall()
+    logger.info("Fetching Budget Drinks")
+    try:
+        # Query the "drinks" table for drinks with a price of 10.99 or lower
+        cur.execute("SELECT * FROM drinks WHERE price::numeric <= 10.99")
+        budget_drinks = cur.fetchall()
 
-    # Convert the query results to a list of dictionaries
-    drink_list = []
-    for drink in budget_drinks:
-        drink_dict = {
-            'drink_id': drink[0],
-            'drink_name': drink[1],
-            'description': drink[2],
-            'price': drink[3],
-            'drink_type': drink[4],
-            'ingredients': drink[5]
-        }
-        drink_list.append(drink_dict)
+        # Convert the query results to a list of dictionaries
+        drink_list = []
+        for drink in budget_drinks:
+            drink_dict = {
+                'drink_id': drink[0],
+                'drink_name': drink[1],
+                'description': drink[2],
+                'price': drink[3],
+                'drink_type': drink[4],
+                'ingredients': drink[5]
+            }
+            drink_list.append(drink_dict)
 
-    return jsonify(drink_list)
+        return jsonify(drink_list)
+    except Exception as e:
+        logger.error(f"Failed to fetch budget drinks: {e}")
+        return "An error occured!", 500
 
 
 # DO NOT TOUCH
