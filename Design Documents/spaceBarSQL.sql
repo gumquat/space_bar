b61cc5196535d71e393ca106fb7095fa833ac62d
evanrich2404 <evanrich2404@gmail.com>
2024-04-14 23:36:49 -0500

    sense we didn't really use this file and is basically just an example of our schema we used I updated it with the current tables

--- Design Documents/spaceBarSQL.sql
+++ Design Documents/spaceBarSQL.sql
@@ -1,8 +1,15 @@
-CREATE TABLE "drinks" (
+CREATE TABLE IF NOT EXISTS "drinks" (
   "drink_id" SERIAL PRIMARY KEY,
-  "drink_name" varchar,
+  "drink_name" varchar UNIQUE,
   "description" varchar,
   "price" varchar,
   "drink_type" varchar,
   "ingredients" varchar[]
+);
+
+CREATE TABLE IF NOT EXISTS "users" (
+  "user_id" SERIAL PRIMARY KEY,
+  "username" varchar UNIQUE NOT NULL,
+  "password" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL
 );
